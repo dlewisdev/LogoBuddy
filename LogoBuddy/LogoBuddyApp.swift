4c88922a7fae6ad8c2a89f73bf754484dbaaf2cf
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-21 20:27:36 -0500

    Initial Commit

---
+++ LogoBuddy/LogoBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogoBuddyApp.swift
+//  LogoBuddy
+//
+//  Created by Danielle Lewis on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LogoBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
